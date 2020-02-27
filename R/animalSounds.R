8dc6e73f1768675bc901c81d0d57e1578d76e6f9
Heather Turner <ht@heatherturner.net>
2020-02-27 21:17:43 +0100

    create animals package

---
+++ R/animalSounds.R
@@ -0,0 +1,6 @@
+animalSounds <- function(animal, sound){
+  assertthat::assert_that(
+    assertthat::is.string(animal),
+    assertthat::is.string(sound))
+  paste0("The ", animal, " goes ", sound, "!")
+}
